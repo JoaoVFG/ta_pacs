909a51be77c77f54990178571858603a054ac87f
Joao Vitor <=>
2018-06-27 11:45:44 -0300

    Criando projeto base - Configurações e dependências

---
+++ src/main/java/br/gov/sp/fatecsjc/TaPacsApplication.java
@@ -0,0 +1,12 @@
+package br.gov.sp.fatecsjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaPacsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaPacsApplication.class, args);
+	}
+}
