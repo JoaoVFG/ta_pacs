006d10f325c271ccd574f33c838122c31255335b
Joao Vitor <=>
2018-06-30 02:33:20 -0300

    Adicionando Edpoint Medicos

--- src/main/java/br/gov/sp/fatecsjc/resources/MedicoResource.java
+++ src/main/java/br/gov/sp/fatecsjc/resources/MedicoResource.java
@@ -1,5 +1,39 @@
 package br.gov.sp.fatecsjc.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.gov.sp.fatecsjc.domain.Medico;
+import br.gov.sp.fatecsjc.services.MedicoService;
+
+@RestController
+@RequestMapping(value = "/medicos")
 public class MedicoResource {
 
+	@Autowired
+	private MedicoService medicoService;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Medico> find(@PathVariable Integer id){
+		
+		Medico medico = medicoService.findById(id);
+		
+		return ResponseEntity.ok().body(medico);
+		
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Medico>> findAll(){
+		List<Medico> medicos = medicoService.findAll();
+		
+		return ResponseEntity.ok().body(medicos);
+	}
+	
 }
