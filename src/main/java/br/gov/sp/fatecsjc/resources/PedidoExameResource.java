de87ed22e49e3c994481fc9796523f5259823be7
Joao Vitor <=>
2018-06-30 02:48:20 -0300

    adicionando endpoit PedidoExame

--- src/main/java/br/gov/sp/fatecsjc/resources/PedidoExameResource.java
+++ src/main/java/br/gov/sp/fatecsjc/resources/PedidoExameResource.java
@@ -1,5 +1,39 @@
 package br.gov.sp.fatecsjc.resources;
 
-public class PedidoExameResource {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.gov.sp.fatecsjc.domain.PedidoExame;
+import br.gov.sp.fatecsjc.services.PedidoExameService;
+
+@RestController
+@RequestMapping(value = "/pedidoexame")
+public class PedidoExameResource {
+	
+	@Autowired
+	private PedidoExameService pedidoExameService;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<PedidoExame> findById(@PathVariable Integer id){
+		
+		PedidoExame pedidoExame = pedidoExameService.findById(id);
+		
+		return ResponseEntity.ok().body(pedidoExame);
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<PedidoExame>> findAll(){
+		
+		List<PedidoExame> pedidosExame = pedidoExameService.findAll();
+		
+		return ResponseEntity.ok().body(pedidosExame);
+	}
+	
 }
