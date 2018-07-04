6ce5e7bd5ee9e124107472d3bdfa09ce84d31b82
Joao Vitor <=>
2018-07-04 09:55:11 -0300

    inserindo testes

--- src/test/java/br/gov/sp/fatecsjc/TaPacsApplicationTests.java
+++ src/test/java/br/gov/sp/fatecsjc/TaPacsApplicationTests.java
@@ -1,16 +1,57 @@
 package br.gov.sp.fatecsjc;
 
+
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.gov.sp.fatecsjc.domain.Exame;
+import br.gov.sp.fatecsjc.services.ExameService;
+import br.gov.sp.fatecsjc.services.MedicoService;
+import br.gov.sp.fatecsjc.services.PacienteService;
+import br.gov.sp.fatecsjc.services.PedidoExameService;
+import junit.framework.TestCase;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TaPacsApplicationTests {
-
+public class TaPacsApplicationTests extends TestCase{
+	
+	@Autowired
+	private ExameService ExameService;
+	
+	@Autowired
+	private PedidoExameService pedidoExameService;
+	
+	@Autowired
+	private MedicoService medicoService;
+	
+	@Autowired
+	private PacienteService pacienteService;
+	
+	
+	
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testaBuscaExameAll() {
+		List<Exame> exame = ExameService.findAll();
+		int qtd = exame.size();
+		assertNotEquals(0, qtd);
+	}
+	
+	@Test
+	public void testaBuscaExameId() {
+		Exame exame = ExameService.findById(1);
+		int id = exame.getId();
+		assertEquals(1, id);
 	}
 
 }
